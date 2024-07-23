ea495c904e9237e72e77299cf615aa071ba3ffa3
Kizark <kiznlh@gmail.com>
2024-07-23 15:14:17 +0700

    add getTokenFromRequest (test later because bug from NATS)

--- go.mod
+++ go.mod
@@ -11,6 +11,9 @@ require (
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/nats-io/nats.go v1.36.0 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 )
--- internal/jwt/jwt.go
+++ internal/jwt/jwt.go
@@ -2,8 +2,10 @@ package jwt
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
+	"github.com/nats-io/nats.go"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/Build-D-An-Ki-n-Truc/auth/internal/config"
@@ -60,6 +62,20 @@ func verifyToken(tokenString string) (jwt.MapClaims, error) {
 	return nil, fmt.Errorf("invalid token")
 }
 
-// func getToken() (string, error){
+// getTokenFromRequest extracts the JWT from the Authorization header of the NATS request.
+func getTokenFromRequest(m *nats.Msg) (string, error) {
+	// Get the Authorization header
+	authHeader := m.Header.Get("Authorization")
+	if authHeader == "" {
+		return "", fmt.Errorf("authorization header missing")
+	}
 
-// }
+	// Split the header to get the token part
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return "", fmt.Errorf("invalid authorization header format")
+	}
+
+	// Return the token part
+	return parts[1], nil
+}
